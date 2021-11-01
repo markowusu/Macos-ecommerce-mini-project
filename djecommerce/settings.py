0e703d720de89dc156cdb1392711b90dc3a478fc
Macos-AI <moa@oamarkets.com>
2021-11-01 00:28:16 +0000

    added: requirements.txt

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -5,7 +5,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = ['https://git.heroku.com/ecommerce-mini-webapp-project.git',
+ALLOWED_HOSTS = ['https://spooky-coffin-82582.herokuapp.com/',
  'localhost' ,
 '127.0.0.1'
 ]
