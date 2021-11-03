82ba42e452ec6626533a2298f79fa60ab63563be
markowusu <markowusuayim@gmail.com>
2021-11-03 16:16:16 +0000

    adding to setting and middleware

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -28,10 +28,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
-
-    'django.contrib.sessions.middleware.SessionMiddleware',
+     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,7 +117,7 @@ STRIPE_SECRETE_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
