2edbb3c8f2de24d7a4e3e023d3c9ea66e06fb780
markowusu <markowusuayim@gmail.com>
2021-11-04 11:58:16 +0000

    changed static files setting

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -105,11 +105,18 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL ='/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
